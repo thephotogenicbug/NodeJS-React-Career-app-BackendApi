f3c5bf751bc541fbd15be4ada5ff7bda7e0fa864
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-08 17:39:53 +0530

    NodeJS: inital backend api for react-career-app

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(express.json())
+app.use(cors())
+
+// MySQL Connection
+const mysql = require('mysql');
+const mydatabase = mysql.createConnection({
+    host: 'localhost',
+    user:  'root',
+    password: '',
+    database: 'career' 
+});
+mydatabase.connect();
+
+// Register function
+app.post("/signup", (req, res) =>{
+  const name = req.body.uname;
+  const email = req.body.email;
+  const mobile = req.body.mobile;
+  const password = req.body.password;
+  const sql = "insert into signup(name, email, mobile, password) values('"+name+"', '"+email+"', '"+mobile+"', '"+password+"')";
+  mydatabase.query(sql, (error, rows, fields)=>{
+      if(error) throw error
+      res.send("Registration Successfull");
+      res.end();
+  })
+})
+
+
+app.listen(4000, function(){
+    console.log("Server is running on port 4000");
+})
