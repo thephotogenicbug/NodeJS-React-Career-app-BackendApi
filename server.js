2217de62c587cea1d5ff6c813c45836572173143
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-09 18:00:32 +0530

    Add post application and get user data mysql query

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post("/signup", (req, res) =>{
 app.post("/login", (req, res)=>{
    const email   = req.body.email;
    const password = req.body.password;
-   const sql = "select * from signup where email='"+email+"', and password='"+password+"'";
+   const sql = "select * from signup where email='"+email+"' and password='"+password+"'";
    mydatabase.query(sql ,(error, rows, fields)=>{
        if(error) throw error
        if(rows.length > 0){
@@ -42,6 +42,47 @@ app.post("/login", (req, res)=>{
            res.end();
        }
    })
+})
+
+app.post('/postapplication', (req,res) =>{
+    const name          = req.body.name;
+    const dob           = req.body.dob;
+    const gender        = req.body.gender;
+    const status        = req.body.status;
+    const mobile        = req.body.mobile;
+    const email         = req.body.email;
+    const sslc          = req.body.sslc;
+    const puc           = req.body.puc;
+    const graduation    = req.body.graduation;
+    const pg            = req.body.pg;
+    const certone       = req.body.certone;
+    const certtwo       = req.body.certtwo;
+    const certthree     = req.body.certthree;
+    const workeone      = req.body.workone;  
+    const worketwo      = req.body.worktwo;  
+    const workethree    = req.body.workthree; 
+    const achone        = req.body.achone; 
+    const achtwo        = req.body.achtwo; 
+    const achthree      = req.body.achthree; 
+    const address       = req.body.address; 
+    const position      = req.body.position;
+    const cid           = req.body.cid
+    const sql = "insert into application(code, name, dob, gender, status, mobile, email, sslc, puc, graduation, pg, certone, certtwo, certthree, workone, worktwo, workthree, achone, achtwo, achthree, address, position) values('"+cid+"', '"+name+"', '"+dob+"', '"+gender+"', '"+status+"', '"+mobile+"', '"+email+"', '"+sslc+"', '"+puc+"', '"+graduation+"', '"+pg+"', '"+certone+"', '"+certtwo+"', '"+certthree+"', '"+workeone+"', '"+worketwo+"', '"+workethree+"', '"+achone+"', '"+achtwo+"', '"+achthree+"', '"+address+"', '"+position+"')";
+    mydatabase.query(sql, (error , rows, fields)=>{
+        if(error) throw error
+        res.send("Data saved successfully...!");
+        res.end();
+    })
+})
+
+app.post('/userdetails', (req, res)=>{
+    const cid  = req.body.cid;
+    const sql = "select * from application where code='"+cid+"' order by id desc";
+    mydatabase.query(sql, (error, rows, fields)=>{
+        if(error) throw error
+          res.send(rows);
+          res.end();
+    })
 })
 
 
